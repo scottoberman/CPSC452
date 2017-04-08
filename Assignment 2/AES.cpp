ba71aa90059ecfd6accef1b5ed8eb17944dd5e87
Gangriz <Gangriz@users.noreply.github.com>
2017-04-07 23:08:54 -0700

    Add files via upload

--- Assignment 2/AES.cpp
+++ Assignment 2/AES.cpp
@@ -1,18 +1,20 @@
-#include "AES.h"
-
-using namespace std;
+﻿#include "AES.h"
 
 /**
- * Sets the key to use
- * @param key - the first byte of this represents whether
- * to encrypt or to decrypt. 00 means encrypt and any other
- * value to decrypt.  Then come the bytes of the 128-bit key
- * (should be 16 of them).
- * @return - True if the key is valid and False otherwise
- */
+* Sets the key to use
+* @param key - the first byte of this represents whether
+* to encrypt or to decrypt. 00 means encrypt and any other
+* value to decrypt.  Then come the bytes of the 128-bit key
+* (should be 16 of them).
+* @return - True if the key is valid and False otherwise
+*/
 bool AES::setKey(const unsigned char* keyArray)
 {
-	
+    this->key = new AES_KEY();
+	int i, setKeyResult;
+	i = setKeyResult = 0;
+	unsigned char keyArrayCopy[16];
+	memcpy(keyArrayCopy, keyArray + 1, 16);
 	// TODO: AES implementation of openssl cares about whether
 	// you are encrypting or decrypting when setting the key.
 	// That is, when encrypting you use function AES_set_encrypt_key(...)
@@ -24,72 +26,57 @@ bool AES::setKey(const unsigned char* keyArray)
 	// Otherwise, use AES_set_decrypt_key(...).  The rest of the bytes in the
 	// array indicate the 16 bytes of the 128-bit AES key.
 	//
+	if (keyArray[0] == '0')
+	{
+		setKeyResult = AES_set_encrypt_key(keyArrayCopy, 128, key);
+                return true;
+	}
+	else
+	{
+		setKeyResult = AES_set_decrypt_key(keyArrayCopy, 128, key);
+                return true;
+	}
 	// Both functions return 0 on success and other values on faliure.
 	// For documentation, please see https://boringssl.googlesource.com/boringssl/+/2623/include/openssl/aes.h
 	// and aes.cpp example provided with the assignment.
-	if (keyArray[0] == '0' && strlen((char*)keyArray) == 17) // If encrypting...
-	{
-		return AES_set_encrypt_key(keyArray + 1, 128, &key);
-	}
-	else if (keyArray[0] == '1' && strlen((char*)keyArray) == 17) // If decrypting...
-	{
-		return AES_set_decrypt_key(keyArray + 1, 128, &key);
-	}
-	else // Error case should not happen
-	{
-		printf("AES::SetKey: Invalid first byte");
-		return false;
-	}	
-}
-
-/**	
- * Encrypts a plaintext string
- * @param plaintext - the plaintext string
- * @return - the encrypted ciphertext string
- */
-unsigned char* AES::encrypt(const unsigned char* plainText)
-{
-	//TODO: 1. Dynamically allocate a block to store the ciphertext.
-	unsigned char* cipherText = new unsigned char[strlen((char*)plainText)];
-	//	2. Use AES_ecb_encrypt(...) to encrypt the text (please see the URL in setKey(...)
-	//	and the aes.cpp example provided.
-
-	// Since AES_ebc_encrypt works on 16 byte blocks, we must
-	// move through the plaintext in 16 byte increments
-	int x;
-	for (x = 0; x < strlen((char*)plainText) - 1; x += 16)
-	{
-		AES_ecb_encrypt(plainText + x, cipherText + x, &key, AES_ENCRYPT);
-	}
-	cipherText[x] = 0;
-	// 	3. Return the pointer to the ciphertext
-		
-	return cipherText;	
+	return false;
 }
 
 /**
- * Decrypts a string of ciphertext
- * @param cipherText - the ciphertext
- * @return - the plaintext
- */
+* Encrypts a plaintext string
+* @param plaintext - the plaintext string
+* @return - the encrypted ciphertext string
+*/
+unsigned char* AES::encrypt(const unsigned char* plainText)
+{
+    
+	//TODO: 1. Dynamically allocate a block to store the ciphertext.
+        
+	unsigned char * ciphertext = NULL;
+	ciphertext = new unsigned char[16];
+	int ciphertextLength = 16;
+	//	2. Use AES_ecb_encrypt(...) to encrypt the text (please see the URL in setKey(...)
+	//	and the aes.cpp example provided.
+	AES_ecb_encrypt(plainText, ciphertext, this->key, AES_ENCRYPT);
+	// 	3. Return the pointer to the ciphertext
+	return ciphertext;
+}
+
+/**
+* Decrypts a string of ciphertext
+* @param cipherText - the ciphertext
+* @return - the plaintext
+*/
 unsigned char* AES::decrypt(const unsigned char* cipherText)
 {
-	
 	//TODO: 1. Dynamically allocate a block to store the plaintext.
-	unsigned char* plainText = new unsigned char[strlen((char*)cipherText)];
+	unsigned char * plaintext = NULL;
+	plaintext = new unsigned char[16];
 	//	2. Use AES_ecb_encrypt(...) to decrypt the text (please see the URL in setKey(...)
 	//	and the aes.cpp example provided.
-
-	// Since AES_ebc_encrypt works on 16 byte blocks, we must
-	// move through the plaintext in 16 byte increments
-	int x;
-	for (x = 0; x < strlen((char*)cipherText) - 1; x += 16)
-	{
-		AES_ecb_encrypt(cipherText + x, plainText + x, &key, AES_DECRYPT);
-	}
-	plainText[x] = 0;
+	AES_ecb_encrypt(cipherText, plaintext, key, AES_DECRYPT);
 	// 	3. Return the pointer to the plaintext
-	return plainText;
+	return plaintext;
 }
 
 
--- Assignment 2/AES.h
+++ Assignment 2/AES.h
@@ -11,47 +11,46 @@
 
 using namespace std;
 
-/** 
- * Implements a AES cipher
- */
-class AES: public CipherInterface
+/**
+* Implements a AES cipher
+*/
+class AES : public CipherInterface
 {
 	/* The public members */
-	public:
-		
-		/**
-		 * The default constructor
-	 	 */
-		AES(){}
-			
-		/**
-		 * Sets the key to use
-		 * @param key - the key to use
-		 * @return - True if the key is valid and False otherwise
-		 */
-		virtual bool setKey(const unsigned char* key);
+public:
 
-		/**	
-		 * Encrypts a plaintext string
-		 * @param plainText - the plaintext string
-		 * @return - the encrypted ciphertext string
-		 */
-		virtual unsigned char* encrypt(const unsigned char* plainText);
+	/**
+	* The default constructor
+	*/
+	AES(){}
 
-		/**
-		 * Decrypts a string of cipherText
-		 * @param ciphertext - the cipherText
-		 * @return - the plaintext
-		 */
-		virtual unsigned char* decrypt(const unsigned char* cipherText);
-	
-			
+	/**
+	* Sets the key to use
+	* @param key - the key to use
+	* @return - True if the key is valid and False otherwise
+	*/
+	virtual bool setKey(const unsigned char* key);
+
+	/**
+	* Encrypts a plaintext string
+	* @param plainText - the plaintext string
+	* @return - the encrypted ciphertext string
+	*/
+	virtual unsigned char* encrypt(const unsigned char* plainText);
+
+	/**
+	* Decrypts a string of cipherText
+	* @param ciphertext - the cipherText
+	* @return - the plaintext
+	*/
+	virtual unsigned char* decrypt(const unsigned char* cipherText);
+
+
 	/* The protected members */
-	protected:
+protected:
 
-		//TODO: declare the AES key
-		AES_KEY key; // Not sure the datatype
+	//TODO: declare the AES key
+	AES_KEY *key;
 };
 
-
-#endif
+#endif // !AES_H
